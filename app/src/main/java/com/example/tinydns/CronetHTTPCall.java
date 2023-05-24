408b1c2dbda52558d71c46600a5110715b384b5f
joojo <joe.reusch92@gmail.com>
2023-05-24 09:17:25 +0200

    Fix cronet setup and remove old URLlib comment.

--- app/src/main/java/com/example/tinydns/CronetHTTPCall.java
+++ app/src/main/java/com/example/tinydns/CronetHTTPCall.java
@@ -6,10 +6,22 @@ import org.chromium.net.CronetException;
 import org.chromium.net.UrlRequest;
 import org.chromium.net.UrlResponseInfo;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
+
 // Implementation taken from https://developer.android.com/guide/topics/connectivity/cronet/start#java
+
 public class CronetHTTPCall extends UrlRequest.Callback {
     private static final String TAG = "tinydns Cronet Stack";
+
+
+    private static final int BYTE_BUFFER_CAPACITY_BYTES = 64 * 1024;
+    private final ByteArrayOutputStream bytesReceived = new ByteArrayOutputStream();
+    private final WritableByteChannel receiveChannel = Channels.newChannel(bytesReceived);
+
 
     @Override
     public void onRedirectReceived(UrlRequest request, UrlResponseInfo info, String newLocationUrl) throws Exception {
@@ -21,22 +33,32 @@ public class CronetHTTPCall extends UrlRequest.Callback {
 
     @Override
     public void onResponseStarted(UrlRequest request, UrlResponseInfo info) throws Exception {
-        Log.i(TAG, "onResponseStarted method called.");
+        int httpStatusCode = info.getHttpStatusCode();
+
+        Log.i(TAG, "onResponseStarted method called. HTTPCode: " + httpStatusCode);
         // You should call the request.read() method before the request can be
         // further processed. The following instruction provides a ByteBuffer object
         // with a capacity of 102400 bytes for the read() method. The same buffer
         // with data is passed to the onReadCompleted() method.
-        request.read(ByteBuffer.allocateDirect(102400));
+        request.read(ByteBuffer.allocateDirect(BYTE_BUFFER_CAPACITY_BYTES));
     }
 
     @Override
     public void onReadCompleted(UrlRequest request, UrlResponseInfo info, ByteBuffer byteBuffer) throws Exception {
         Log.i(TAG, "onReadCompleted method called.");
+        // This gets called once the first read in onResponseStarted is completed and will keep
+        // getting called until the transmission is completed.
         // You should keep reading the request until there's no more data.
         // Flip the buffer from network order.
         byteBuffer.flip();
 
         // Read into the channel.
+        try {
+            receiveChannel.write(byteBuffer);
+        } catch (IOException e) {
+            android.util.Log.i(TAG, "IOException during ByteBuffer read. Details: ", e);
+        }
+        // Prepare buffer for next read.
         byteBuffer.clear();
         request.read(byteBuffer);
     }
@@ -45,7 +67,12 @@ public class CronetHTTPCall extends UrlRequest.Callback {
     public void onSucceeded(UrlRequest request, UrlResponseInfo info) {
         Log.i(TAG, "onSucceeded method called.");
         //val bodyBytes = bytesReceived.toByteArray()
+        byte[] bodyBytes = bytesReceived.toByteArray();
+        onSucceeded(request, info, bodyBytes);
         //onSucceeded(request, info, bodyBytes)
+    }
+
+    public void onSucceeded(UrlRequest request, UrlResponseInfo info, byte[] bodyBytes) {
     }
 
     @Override
--- app/src/main/java/com/example/tinydns/MainActivity.java
+++ app/src/main/java/com/example/tinydns/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import org.chromium.net.CronetEngine;
 import org.chromium.net.UrlRequest;
+import org.chromium.net.UrlResponseInfo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,51 +28,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textfield = (TextView) findViewById(R.id.textfield);
+        textfield.setText("reqContent");
         // Create Cronet Enginge for HTTP requests.
         this.cronetEngine = new CronetEngine.Builder(this).build();
-
         croExecutor = Executors.newSingleThreadExecutor();
         ByteBuffer reqBuffer = ByteBuffer.allocate(400);
-        UrlRequest.Builder requestBuilder = cronetEngine.newUrlRequestBuilder(
-                "https://www.example.com", new CronetHTTPCall(), croExecutor);
+        CronetHTTPCall cronetCallback = new CronetHTTPCall() {
+            @Override
+            public void onSucceeded(UrlRequest request, UrlResponseInfo info, byte[] bodyBytes) {
+                // Parse the request result and update the UI.
+            }
+        };
+        UrlRequest.Builder requestBuilder = cronetEngine.newUrlRequestBuilder("https://www.example.com", cronetCallback, croExecutor);
         UrlRequest request = requestBuilder.build();
         request.start();
-        request.read(reqBuffer);
-        byte[] data = new byte[reqBuffer.remaining()]; // Create a byte array to hold the data
-        reqBuffer.get(data); // Read bytes into the byte array
-        String reqContent = new String(data, StandardCharsets.UTF_8);
-
-        textfield = (TextView) findViewById(R.id.textfield);
-        textfield.setText(reqContent);
-
-//        try {
-//            textfield.setText("now here");
-//            URL url = new URL("http://127.0.0.1:5000");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("GET");
-//            connection.getResponseCode();
-//
-//            StringBuilder response = new StringBuilder();
-//            if (respCode == HttpURLConnection.HTTP_OK){
-//                textfield.setText("then here" + respCode);
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                String line;
-//                textfield.setText("then here" + 12);
-//                textfield.setText("we actually get here");
-//                while ((line = reader.readLine()) != null) {
-//                    response.append(line);
-//                }
-//            } else {
-//                response.append("Error" + respCode);
-//            }
-//            textfield.setText(response);
-//
-//        }   catch (IOException e) {
-//            System.out.println(e);
-//        }
-
-
     }
     public CronetEngine getCronetEngine() {
         return cronetEngine;
